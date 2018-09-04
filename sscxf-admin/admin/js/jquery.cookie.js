cb73bfcb776b1eb10fd212a27fde578ba64976f7
kuchensheng <787211647@qq.com>
2018-09-05 00:00:28 +0800

    Merge branch 'develop' of https://github.com/kuchensheng/sscxf-admin into develop
    # Conflicts:
    #	sscxf-admin/admin/index.html

--- sscxf-admin/admin/js/jquery.cookie.js
+++ sscxf-admin/admin/js/jquery.cookie.js
@@ -135,3 +135,15 @@
 {try{window.sessionStorage.removeItem(key);}catch(e){};this._deleteFallback(key);return this;},_clearSession:function()
 {try{window.sessionStorage.clear();}catch(e){for(var i in window.sessionStorage){window.sessionStorage.removeItem(i);}}},clear:function()
 {this._clearSession();this._clearFallback();return this;}};$.session._init();})(jQuery);
+
+function timeStamp2String(time){
+    var datetime = new Date();
+    datetime.setTime(time);
+    var year = datetime.getFullYear();
+    var month = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
+    var date = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
+    var hour = datetime.getHours()< 10 ? "0" + datetime.getHours() : datetime.getHours();
+    var minute = datetime.getMinutes()< 10 ? "0" + datetime.getMinutes() : datetime.getMinutes();
+    var second = datetime.getSeconds()< 10 ? "0" + datetime.getSeconds() : datetime.getSeconds();
+    return year + "-" + month + "-" + date+" "+hour+":"+minute+":"+second;
+}
